f38da02a1bebd883b38d35d0c0900a154fa30d66
Raphael Freitas Nogueira de Souza <raphaelfreitas18.dev@gmail.com>
2022-12-20 14:57:44 -0300

    Added the get by id and get by email User Routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const prisma = require('../database/config')
-
 const router = express.Router();
+
+
+//GET ROUTES
+
+//GET all
 router.get('/', async (req, res) => {
   try {
     const users = await prisma.user.findMany({})
@@ -10,6 +14,50 @@ router.get('/', async (req, res) => {
     res.status(500).json(error)
   }
 })
+
+//GET by id
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const user = await prisma.user.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    })
+
+    if (user) {
+      res.status(200).json(user)
+    } else {
+      res.status(200).json({message: 'No user found for this id'})
+    }
+
+  } catch (error) {
+    res.status(500).json(error)
+    console.log(error);
+  }
+})
+
+//GET by email
+router.get('/byemail/:email', async (req, res) => {
+  try {
+    const { email } = req.params
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    })
+    
+    if (user) {
+      res.status(200).json(user)
+    } else {
+      res.status(200).json({message: 'No user found for this email'})
+    }
+
+} catch (error) {
+  res.send(500).json(error)
+}
+})
+
 
 router.post('/', async (req, res) => {
   try {
@@ -20,7 +68,7 @@ router.post('/', async (req, res) => {
         email: email,
       },
     })
-    res.status(200).json(user)
+    res.status(201).json(user)
   } catch (error) {
     res.status(500).json(error)
   }
