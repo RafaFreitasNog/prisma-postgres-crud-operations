79b456a9b5a31e7f38637299bd054cbcefe07921
Raphael Freitas Nogueira de Souza <raphaelfreitas18.dev@gmail.com>
2023-03-09 10:37:22 -0300

    Added a bug fix that corrected the crashing behaviour if a deleted user token was sent

--- src/middlewares/autheticate.js
+++ src/middlewares/autheticate.js
@@ -6,20 +6,24 @@ const authenticateUser = async (req, res, next) => {
   const token = req.headers['authtoken']
 
   if (!token) {
-    res.status(401).json({message: 'Unauthorized: no token provided'})
+    res.status(401).json({ message: 'Unauthorized: no token provided' })
   } else {
     jwt.verify(token, jwtSecret, async (err, decoded) => {
       if (err) {
-        res.status(401).json({message: 'Unauthorized: invalid token'})
+        res.status(401).json({ message: 'Unauthorized: invalid token' })
       } else {
         const findUser = await prisma.user.findUnique({
           where: {
             id: decoded.id,
           },
         })
-        delete findUser.password;
-        req.reqUser = findUser
-        next()
+        if (findUser) {
+          delete findUser.password;
+          req.reqUser = findUser
+          next()
+        } else {
+          res.status(401).json({ message: 'No user Found for this session token' })
+        }
       }
     })
   }
